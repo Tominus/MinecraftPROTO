7d90c19d93e394672ac3f761788dfae44644353f
Tominus <Tominusstarwars@gmail.com>
2022-06-21 11:11:37 +0200

    2

---
+++ MinecraftPROTO/CMakeFiles/launch-tutorial07_model_loading-MinSizeRel.cmd
@@ -0,0 +1,9 @@
+cd D:/P2/MinecraftPROTO/ogl-master/tutorial07_model_loading/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"D:\P2\MinecraftPROTO\MinecraftPROTO\MinSizeRel\tutorial07_model_loading.exe"  
+pause
---
+++ MinecraftPROTO/CMakeFiles/launch-tutorial07_model_loading-RelWithDebInfo.cmd
@@ -0,0 +1,9 @@
+cd D:/P2/MinecraftPROTO/ogl-master/tutorial07_model_loading/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"D:\P2\MinecraftPROTO\MinecraftPROTO\RelWithDebInfo\tutorial07_model_loading.exe"  
+pause
