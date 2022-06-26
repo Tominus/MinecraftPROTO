7ba86085db7bbf0d598d2857cd93a879129e144c
Tominus <Tominusstarwars@gmail.com>
2022-06-26 17:03:33 +0200

    add gitignore on MinecraftPROTO

--- MinecraftPROTO/launch-tutorial07_model_loading-Release.cmd
+++
@@ -1,9 +0,0 @@
-cd E:/Github/MinecraftPROTO/ogl-master/tutorial07_model_loading/
-set 
-
-if [%1]==[--debugger] (
-	SHIFT
-)
-
-"E:\Github\MinecraftPROTO\MinecraftPROTO\Release\tutorial07_model_loading.exe"  
-pause
